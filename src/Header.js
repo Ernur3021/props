6550eca494337aa2f22eb76d489db96b82030811
Ernur3021 <ernuromarer@gmail.com>
2023-08-29 00:26:04 +0600

    fix bag class

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+
+
+function Header() {
+    return(
+        <header>
+             <nav>
+                 <div className="logo">LOGO</div>
+                    <ul className="wrapper">
+                        <li className="wrap">Home</li>
+                        <li className="wrap">About us</li>
+                        <li className="wrap">Contact</li>
+                        <li className="wrap">History</li>
+                    </ul>
+                    <div className="btns">
+
+                    </div>
+                </nav>
+        </header>
+    )
+}
+
+
+
+
+export default Header
